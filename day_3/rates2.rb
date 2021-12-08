79ef2d463c5e2c8a678266aa6c33437d67b7069a
Michael <mgirvan838@gmail.com>
2021-12-08 01:10:28 +0000

    Day 3 solved.

--- day_3/rates2.rb
+++ day_3/rates2.rb
@@ -1,44 +1,28 @@
-data = File.open('data.txt', 'r').readlines.map { |line| line[0..-2] }
+data = File.open('data.txt', 'r').readlines.map { |line| line.chomp }
 
-reps = data[0].length
 o2 = data.dup
 co2 = data.dup
 
-reps.times do |i|
-  sum = 0
-  o2.each do |line|
-    sum += Integer(line[i], 10)
+i = 0
+while o2.length > 1
+  ones = o2.reduce(0) { |sum, num| sum + num[i].to_i }
+  if ones >= o2.length / 2.0
+    o2 = o2.select { |num| num[i] == '1' }
+  else
+    o2 = o2.select { |num| num[i] == '0' }
   end
-
-  total = o2.length
-  puts "sum: #{sum}, total: #{total}"
-  o2 = o2.select do |line|
-    if o2.length == 1
-      line
-    elsif sum == total / 2
-      line[i] == '1'
-    else
-      line[i] == (sum > total / 2 ? '1' : '0')
-    end
-  end
-
-  sum = 0
-  co2.each do |li|
-    sum += Integer(li[i], 10)
-  end
-
-  total = co2.length
-  puts "sum: #{sum}, total: #{total}"
-  co2 = co2.select do |li|
-    if co2.length == 1
-      li
-    elsif sum == total / 2
-      li[i] == '0'
-    else
-      li[i] == ((sum < total / 2) ? '1' : '0')
-    end
-  end
+  i += 1
 end
 
-p o2, co2
+i = 0
+while co2.length > 1
+  ones = co2.reduce(0) { |sum, num| sum + num[i].to_i }
+  if ones >= co2.length / 2.0
+    co2 = co2.select { |num| num[i] == '0' }
+  else
+    co2 = co2.select { |num| num[i] == '1' }
+  end
+  i += 1
+end
+
 p o2[0].to_i(2) * co2[0].to_i(2)
