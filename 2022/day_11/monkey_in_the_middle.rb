15335ca51a04a51ad16d10605b1a20563e0a48bc
Michael Girvan <mgirvan838@gmail.com>
2022-12-16 16:58:18 +0000

    Day 11 complete Solution very messy, should revisit

--- 2022/day_11/monkey_in_the_middle.rb
+++ 2022/day_11/monkey_in_the_middle.rb
@@ -5,9 +5,11 @@ DATA.each_slice(7).with_index do |monkey, i|
 	$monkeys[i] = {}
 	$monkeys[i][:items] = monkey[1].scan(/\d+/).map(&:to_i)
 	$monkeys[i][:op] = monkey[2].scan(/=.+/)[0]
+	$monkeys[i][:sign] = $monkeys[i][:op].scan(/(\+|\*)/).flatten[0].to_sym
 	$monkeys[i][:divisor] = monkey[3].scan(/\d+/)[0].to_i
 	$monkeys[i][:receivers] = [monkey[4][-1].to_i, monkey[5][-1].to_i]
 end
+LIMIT = $monkeys.reduce(1) { |acc, monkey| acc * monkey[:divisor] }
 
 def one
 	inspections = Array.new($monkeys.length) { 0 }
@@ -34,17 +36,16 @@ def two
 			until $monkeys[i][:items].empty?
 				inspections[i] += 1
 				item = $monkeys[i][:items].shift
-				sign = $monkeys[i][:op].scan(/(\+|\*)/).flatten[0]
 				op = $monkeys[i][:op].gsub "old", item.to_s
-				item = op.scan(/\d+/).map(&:to_i).reduce(sign.to_sym)
-				item = [item, 10_000].max
+				old = item.dup
+				item = op.scan(/\d+/).map(&:to_i).reduce($monkeys[i][:sign])
 				recipient = item % $monkeys[i][:divisor] == 0 ? 0 : 1
+				item = item % LIMIT
 				$monkeys[$monkeys[i][:receivers][recipient]][:items] << item
 			end
 		end
 	end
-	puts inspections.sort[-2..-1].reduce(:*)
+	puts inspections.sort.reverse[0..1].reduce(:*)
 end
 
-one
 two
