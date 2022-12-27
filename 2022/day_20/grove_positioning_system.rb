ccff6b2c59ff3a5e6053a849d03c88768c890833
Michael Girvan <michael@michaelgirvan.co.uk>
2022-12-27 20:31:31 +0000

    Day 20 complete Decided to start over with a much neater solution, which happily ended up being correct

--- 2022/day_20/grove_positioning_system.rb
+++ 2022/day_20/grove_positioning_system.rb
@@ -1,35 +1,37 @@
-require "pry"
 DATA = File.open(ARGV[0], "r").readlines.map { |num| num.chomp.to_i }
 
-def one
-	order = Array.new(DATA.length) { |i| i }
-	p DATA.length
-	(DATA.length).times do |i|
-		current = order.index(i)
-		item = DATA[current]
-		dest = (item + current + (item < 0 ? 0 : 1)) % (DATA.length - 1)
-		origin = order.index(i)
-		if dest.zero?
-			DATA.push(item)
-			DATA.delete_at(origin)
-			order.push(current)
-			order.delete_at(origin)
-		elsif dest == DATA.length - 1
-			DATA.unshift(item)
-			DATA.delete_at(origin + 1)
-			order.unshift(current)
-			order.delete_at(origin + 1)
-		else
-			DATA.insert(dest, item) 
-			DATA.delete_at(origin + (dest < origin ? 1 : 0))
-			order.insert(dest, current)
-			order.delete_at(origin + (dest < origin ? 1 : 0))
-		end
+def one array
+	orders = Array.new(array.length) { |i| i }
+	(array.length).times do |i|
+		current_pos = orders.index(i)
+		current_num = array[current_pos]
+		array.delete_at(current_pos)
+		dest = (current_num + current_pos) % array.length
+		array.insert(dest, current_num)
+		orders.delete_at(current_pos)
+		orders.insert(dest, i)
 	end
-	zero_pos = DATA.index(0)
-	p DATA.length
-	p [DATA[(zero_pos + 1000) % DATA.length], DATA[(zero_pos + 2000) % DATA.length], DATA[(zero_pos + 3000) % DATA.length]]
-	return [DATA[(zero_pos + 1000) % DATA.length], DATA[(zero_pos + 2000) % DATA.length], DATA[(zero_pos + 3000) % DATA.length]].sum
+	zero_pos = array.index(0)
+	return [array[(zero_pos + 1000) % array.length], array[(zero_pos + 2000) % array.length], array[(zero_pos + 3000) % array.length]].sum
 end
 
-puts one
+def two array
+	orders = Array.new(array.length) { |i| i }
+	array.map! { |num| num * 811589153 }
+	10.times do
+		(array.length).times do |i|
+			current_pos = orders.index(i)
+			current_num = array[current_pos]
+			array.delete_at(current_pos)
+			dest = (current_num + current_pos) % array.length
+			array.insert(dest, current_num)
+			orders.delete_at(current_pos)
+			orders.insert(dest, i)
+		end
+	end
+	zero_pos = array.index(0)
+	return [array[(zero_pos + 1000) % array.length], array[(zero_pos + 2000) % array.length], array[(zero_pos + 3000) % array.length]].sum
+end
+
+puts one(DATA.dup)
+puts two(DATA.dup)
